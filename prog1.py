49d02f3d1453865a4e663f8320a4ce40c22da8c7
SheetalAgarwal <Sheetal0797@gmail.com>
2023-03-03 16:46:10 +0530

    calci2

---
+++ prog1.py
@@ -0,0 +1,6 @@
+#!/use/bin/python3
+#source code for summation of two numbers
+
+def summation(data):
+	print("data")
+	return sum(data)
---
+++ test.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+#test case for adding two numbers
+import unittest
+from prog1 import summation
+class TestSum(unittest.TestCase):
+	def test_list_int(self):
+		"""
+		test case to add two numbers
+		"""
+		print("test")
+		data=[23,32]
+		result=summation(data)
+		self.assertEqual(result,55)
+if __name__=='__main__':
+	unittest.main()
