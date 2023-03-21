f395d4c892e42ff22284dfcf8970c056a6e95585
SheetalAgarwal <Sheetal0797@gmail.com>
2023-03-21 19:57:11 +0530

    calculator

--- src/main/java/SciCal.java
+++ src/main/java/SciCal.java
@@ -1,20 +1,95 @@
+import java.util.InputMismatchException;
 import java.util.Scanner;
-public class SciCal {
-    public static void main(String [] args) {
-        //System.out.println("Hello world");
-        SciCal obj=new SciCal();
-        System.out.print("Enter a number: ");
-        Scanner scanner = new Scanner(System.in);
-        int n=scanner.nextInt();
-        System.out.println(obj.factorial(n));
-    }
-    public int factorial(int n)
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class SciCal
+{
+    private static final Logger logger= LogManager.getLogger(SciCal.class);
+    public SciCal(){}
+    public static void main(String [] args)
     {
-        int ans=1;
-        for(int i=1;i<=n;i++)
-        {
-            ans=ans*i;
-        }
-        return ans;
+        SciCal obj=new SciCal();
+        Scanner scanner = new Scanner(System.in);
+        double number1, number2;
+
+        do {
+            System.out.println("Calculator-using-DevOps, Choose to perform operation");
+            System.out.print("Press 1 to find Square Root\nPress 2 to find Factorial\nPress 3 to find Natural Log\nPress 4 to find Power\n" +
+                    "Press 5 to exit\nEnter your choice: ");
+            int choice;
+            try {
+                choice = scanner.nextInt();
+            } catch (InputMismatchException error) {
+                return;
+            }
+
+            switch (choice)
+            {
+                case 1:
+                    // do Square root
+                    System.out.print("Enter the first number : ");
+                    number1 = scanner.nextDouble();
+                    System.out.println("Square root of "+number1+" is : " + obj.sqrt(number1));
+                    System.out.println("\n");
+                    break;
+
+                case 2:
+                    // do Factorial
+                    System.out.print("Enter the first number : ");
+                    number1 = scanner.nextDouble();
+                    System.out.println("Factorial of "+number1+" is : " + obj.factorial(number1));
+                    System.out.println("\n");
+                    break;
+
+                case 3:
+                    // do Natural Log
+                    System.out.print("Enter the first number : ");
+                    number1 = scanner.nextDouble();
+                    System.out.println("Natural Log of "+number1+" is : " + obj.naturallog(number1));
+                    System.out.println("\n");
+                    break;
+
+                case 4:
+                    // do Power
+                    System.out.print("Enter the first number : ");
+                    number1 = scanner.nextDouble();
+                    System.out.print("Enter the second number : ");
+                    number2 = scanner.nextDouble();
+                    System.out.println(number1+" raised to the power "+number2+" is : " + obj.power(number1,number2));
+                    System.out.println("\n");
+                    break;
+                default:
+                    System.out.println("Exiting....");
+                    return;
+            }
+        } while (true);
+    }
+    public double sqrt(double number1) {
+        logger.info("[SQUARE ROOT] - " + number1);
+        double sqr = Math.sqrt(number1);
+        double result = sqr;
+        logger.info("[RESULT - SQUARE ROOT] - " + result);
+        return result;
+    }
+
+    public double factorial(double number1){
+        logger.info("[FACTORIAL] - " + number1);
+        double result = (number1==1 || number1==0) ? 1:number1*factorial(number1-1);
+        logger.info("[RESULT - FACTORIAL] - " + result);
+        return result;
+    }
+
+    public double naturallog(double number1){
+        logger.info("[NATURAL LOGARITHM] - " + number1);
+        double result = Math.log(number1);
+        logger.info("[RESULT - NATURAL LOGARITHM - " + result);
+        return result;
+    }
+    public double power(double number1, double number2){
+        logger.info("[POWER FUNCTION] - " + number1 + "RAISED TO THE POWER" + number2);
+        double result = Math.pow(number1, number2);
+        logger.info("[RESULT - POWER FUNCTION - " + result);
+        return result;
     }
 }
--- src/test/java/SciCalTest.java
+++ src/test/java/SciCalTest.java
@@ -1,13 +1,70 @@
 import org.junit.Test;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
+
 public class SciCalTest {
+
+    private static final double DELTA = 1e-15;
     SciCal obj=new SciCal();
+    @Test
+    public void sqrtTruePositive()
+    {
+        assertEquals("Finding square root of a number for True Positive", 2, obj.sqrt(4), DELTA);
+        assertEquals("Finding square root of a number for True Positive", 10, obj.sqrt(100), DELTA);
+
+    }
+
+    @Test
+    public void sqrtFalsePositive()
+    {
+        assertNotEquals("Finding square root of a number for False Positive", 4, obj.sqrt(25), DELTA);
+        assertNotEquals("Finding square root of a number for False Positive", 100, obj.sqrt(4), DELTA);
+    }
+
     @Test
     public void factorialTruePositive()
     {
-        assertEquals("Finding factorial of a number for True Positive",120,obj.factorial(5));
-        assertEquals("Finding factorial of a number for True Positive",24,obj.factorial(4));
+        assertEquals("Finding factorial of a number for True Positive", 120, obj.factorial(5), DELTA);
+        assertEquals("Finding factorial of a number for True Positive", 24, obj.factorial(4), DELTA);
 
+    }
+
+    @Test
+    public void factorialFalsePositive()
+    {
+        assertNotEquals("Finding factorial of a number for False Positive", 120, obj.factorial(3), DELTA);
+        assertNotEquals("Finding factorial of a number for False Positive", 100, obj.factorial(4), DELTA);
+    }
+
+
+    @Test
+    public void naturallogTruePositive()
+    {
+        assertEquals("Finding natural log of a number for True Positive", 0, obj.naturallog(1), DELTA);
+        assertEquals("Finding factorial of a number for True Positive", 2.302585092994046, obj.naturallog(10), DELTA);
+
+    }
+
+    @Test
+    public void naturallogFalsePositive()
+    {
+        assertNotEquals("Finding natural log of a number for False Positive", 120, obj.naturallog(1), DELTA);
+        assertNotEquals("Finding natural log of a number for False Positive", 4, obj.naturallog(100), DELTA);
+    }
+
+    @Test
+    public void powerTruePositive()
+    {
+        assertEquals("Finding power of a number for True Positive", 25, obj.power(5, 2), DELTA);
+        assertEquals("Finding power of a number for True Positive", 256, obj.power(4, 4), DELTA);
+
+    }
+
+    @Test
+    public void powerFalsePositive()
+    {
+        assertNotEquals("Finding power of a number for False Positive", 10, obj.power(3, 2), DELTA);
+        assertNotEquals("Finding power of a number for False Positive", 25, obj.power(4,2), DELTA);
     }
 
 }
